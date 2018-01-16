6560d829e20bbd9fb0d9549f14989273bed9d90a
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2018-01-16 23:57:04 +0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# microbenz.github.io
-This project using **Gatsby** static-site generator for React. Deploy on GitHub pages.
--- src/components/Shared/Container.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const SectionContainer = styled.div`
-  padding: 40px 0;
-  @media(max-width: 768px) {
-    padding: 40px 25px 20px;
-    ${props => props.isLast && 'padding-bottom: 40px;'}
-  }
-`;

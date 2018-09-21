3453c2fd60e5224f4b8f17c45c6317ea8ade0be6
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2018-09-21 16:51:45 +0700

    Upgrade styled version

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-sass',
+    'gatsby-plugin-webpack-bundle-analyzer',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
